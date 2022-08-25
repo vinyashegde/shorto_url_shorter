a1acd50b4cbe1c6a080eebbd46c7b7e5e5545e60
vinyashegde <vinyasvasanthegde7@gmail.com>
2022-08-26 03:54:56 +0530

    update

---
+++ url shortner/option.js
@@ -0,0 +1,21 @@
+let saveBtn = document.querySelector("#save");
+let api = document.querySelector("#myapi")
+let toastError = document.querySelector('.toast-error')
+let toastSuccess = document.querySelector('.toast-success')
+
+saveBtn.addEventListener('click', () => {
+    if (api.value) {
+        toastSuccess.classList.remove('d-hide');
+
+        // storing the data in extension
+        chrome.storage.local.set({ "API": api.value }, function () {
+            console.log('Value is set to ' + api.value);
+        });
+
+    } else {
+        toastError.classList.remove('d-hide')
+        setTimeout(() => {
+            toastError.classList.add('d-hide')
+        }, 1500)
+    }
+})
